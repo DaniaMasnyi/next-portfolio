a77afeb5fcbea1253cd23fc411d652ea1a503d6e
DaniaMasnyi <88318336+DaniaMasnyi@users.noreply.github.com>
2024-11-20 19:31:13 +0200

    better text

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -53,27 +53,27 @@ export const HeroSection = () => {
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 mt-[-2%] inline-flex items-center gap-4 rounded-lg">
             <div className="bg-green-500 size-2.5 rounded-full"></div>
             <div className="text-sm font-medium">
-              Available for new projects
+              Available to bring your vision to life
             </div>
           </div>
         </div>
         <div className="max-w-lg mx-auto">
           <h1 className="font-serif text-3xl text-center mt-8 tracking-wide md:text-5xl">
-            Building Exceptional User Experiences
+            Creating Impactful Online Experiences
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
-            I specialize in transforming designs into functional,
-            high-performing web applications. Let's discuss your next project.
+            Turning designs into high-performance web platforms. Let’s discuss
+            your next project.
           </p>
         </div>
         <div className="flex flex-col items-center mt-8 gap-4 md:flex-row justify-center">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
-            <span className="font-semibold">Explore My Work</span>
+            <span className="font-semibold">Discover My Work</span>
             <ArrowDown className="size-4" />
           </button>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span>👋🏻</span>
-            <span className="font-semibold">Let's Connect</span>
+            <span className="font-semibold">Reach Out Today</span>
           </button>
         </div>
       </div>
