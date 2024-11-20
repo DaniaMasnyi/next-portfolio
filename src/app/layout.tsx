d8a4981a730a00c20870c666d39601e8fc2fe32b
DaniaMasnyi <88318336+DaniaMasnyi@users.noreply.github.com>
2024-11-20 16:06:15 +0200

    prettier added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
 import type { Metadata } from "next";
-import {Inter, Calistoga} from 'next/font/google';
+import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
-import {twMerge} from "tailwind-merge";
+import { twMerge } from "tailwind-merge";
 
-const inter = Inter({subsets: ['latin'], variable: '--font-sans' });
-const calistoga = Calistoga({subsets: ['latin'], variable: '--font-serif', weight: ["400"]});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
-    title: "My Portfolio",
-    description: "Created with the help of Frontend Tribe",
+  title: "My Portfolio",
+  description: "Created with the help of Frontend Tribe",
 };
 
 export default function RootLayout({
@@ -18,7 +22,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={twMerge(inter.className, calistoga.className, "bg-gray-900 text-white antialiased")}>{children}</body>
+      <body
+        className={twMerge(
+          inter.className,
+          calistoga.className,
+          "bg-gray-900 text-white antialiased",
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
