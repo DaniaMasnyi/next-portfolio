012b6c5d18a8feb714d13a6540dfbe7863ac0308
DaniaMasnyi <88318336+DaniaMasnyi@users.noreply.github.com>
2024-11-20 19:20:19 +0200

    hero section is finished

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from "next";
-import { Inter, Calistoga } from "next/font/google";
-import "./globals.css";
-import { twMerge } from "tailwind-merge";
+import type { Metadata } from 'next';
+import { Inter, Calistoga } from 'next/font/google';
+import './globals.css';
+import { twMerge } from 'tailwind-merge';
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 const calistoga = Calistoga({
-  subsets: ["latin"],
-  variable: "--font-serif",
-  weight: ["400"],
+  subsets: ['latin'],
+  variable: '--font-serif',
+  weight: ['400'],
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: 'My Portfolio',
+  description: 'Created with the help of Frontend Tribe',
 };
 
 export default function RootLayout({
@@ -26,7 +26,7 @@ export default function RootLayout({
         className={twMerge(
           inter.variable,
           calistoga.variable,
-          "bg-gray-900 text-white antialiasing font-sans",
+          'bg-gray-900 text-white antialiasing font-sans',
         )}
       >
         {children}
